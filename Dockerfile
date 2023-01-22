0680ee377034a4788227feb7c5f1703b21b262f0
Aaron Stafford <inthebox@nacious.net>
2023-01-22 05:05:54 +0000

    Added patch needed to build for multiple architectures. Updated scripts to use new patch

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,7 @@ ARG DEBIAN_FRONTEND=noninteractive
 
 RUN apt update
 RUN apt install -y curl 
+RUN apt install -y python-is-python3
 RUN apt install -y git 
 RUN apt install -y unzip
 RUN apt install -y build-essential
--- script/runner.sh
+++ script/runner.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
+set -e
 
 git clone https://github.com/Unity-Technologies/com.unity.webrtc.git --depth=1
 cd com.unity.webrtc
-patch -N BuildScripts~/build_plugin_android.sh < /patches/x86_64.patch
-cp /result/webrtc-android.zip .
-bash BuildScripts~/build_plugin_android.sh
+patch -N BuildScripts~/build_plugin_android.sh < /patches/all.patch
+cp /result/webrtc-android.zip webrtc.zip
+bash -e BuildScripts~/build_plugin_android.sh
