eb0a6d8edfafbb5f2fa5fdda6114a3ae62faf735
Aaron Stafford <cyangpt@nacious.net>
2023-01-23 15:51:24 +0000

    Script now does checkout of unity repo into result folder since the result needs to be kept and potentially modified/committed.

--- script/runner.sh
+++ script/runner.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -e
-
+cd /result
 git clone https://github.com/Unity-Technologies/com.unity.webrtc.git --depth=1
 cd com.unity.webrtc
 patch -N BuildScripts~/build_plugin_android.sh < /patches/all.patch
