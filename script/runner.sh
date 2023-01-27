3472831d850f3854f6f9f8a7d046dfe906888d8e
Aaron Stafford <toottoot@nacious.net>
2023-01-27 01:48:29 +0000

    Changed the variable name used to represent the arch abi to better reflect what it is. Added a patch to help cmake file lib for x64

--- script/runner.sh
+++ script/runner.sh
@@ -3,6 +3,6 @@ set -e
 cd /result
 git clone https://github.com/Unity-Technologies/com.unity.webrtc.git --depth=1
 cd com.unity.webrtc
-patch -N BuildScripts~/build_plugin_android.sh < /patches/all.patch
+patch -p1 -i /patches/all.patch
 cp /result/webrtc-android.zip webrtc.zip
 bash -e BuildScripts~/build_plugin_android.sh
