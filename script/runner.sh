5f5d61958dd4b47d1daa44ca4fe1bb3827630df8
DrStaf <aaron@staffordfarm.com.au>
2023-06-07 19:18:54 -0400

    Update runner.sh

--- script/runner.sh
+++ script/runner.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -e
-git clone --branch issue-802-part-1 https://github.com/lumin-leaper/com.unity.webrtc --depth=1
+git clone --branch issue-802-part-2 https://github.com/lumin-leaper/com.unity.webrtc --depth=1
 cd com.unity.webrtc
 cp /result/webrtc-android.zip webrtc.zip
 bash -e BuildScripts~/build_plugin_android.sh
