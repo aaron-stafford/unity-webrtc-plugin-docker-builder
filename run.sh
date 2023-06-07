d13cc0721dae7bcbdb4f66b14eedfa151f1ff206
DrStaf <aaron@staffordfarm.com.au>
2023-06-07 19:48:56 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,2 @@
 #!/usr/bin/env bash
-
-docker run --platform=linux/amd64 -v $(pwd)/patches:/patches -v $(pwd)/script:/script -v $(pwd)/result:/result -it build_unity_webrtc_plugin_android
+docker run --platform=linux/amd64 -v $(pwd)/patches:/patches -v $(pwd)/script:/script -v $(pwd)/result:/result -it build_unity_webrtc_plugin_android ./script/runner.sh all
