198de074faea81aef5320d099dc06927732307d9
DrStaf <dark-apple@nacious.net>
2023-06-07 22:58:28 -0400

    Update run-debug.sh

--- run-debug.sh
+++ run-debug.sh
@@ -1,2 +1,4 @@
 #!/usr/bin/env bash
+set -e
+set -x
 docker run --platform=linux/amd64 -v $(pwd)/patches:/patches -v $(pwd)/script:/script -v $(pwd)/result:/result -it build_unity_webrtc_plugin_android
