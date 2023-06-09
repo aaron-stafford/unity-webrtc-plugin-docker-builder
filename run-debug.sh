9da4cb3223f84dce79583571d830020adc9065ce
DrStaf <dark-apple@nacious.net>
2023-06-09 19:56:22 -0400

    Update run-debug.sh

--- run-debug.sh
+++ run-debug.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -e
 set -x
-docker run --platform=linux/amd64 -v $(pwd)/script:/script -v $(pwd)/../result:/result -it build_unity_webrtc_plugin_android
+docker run --env WEBRTC_GIT_REPO=$WEBRTC_PLUGIN_REPO --env WEBRTC_GIT_REPO_BRANCH=$WEBRTC_PLUGIN_REPO_BRANCH --platform=linux/amd64 -v $(pwd)/script:/script -v $(pwd)/../result:/result -it build_unity_webrtc_plugin_android
