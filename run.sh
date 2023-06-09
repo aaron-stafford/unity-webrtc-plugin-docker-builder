df9344a94b582635e182b064ec6bb3af7f630022
DrStaf <dark-apple@nacious.net>
2023-06-08 20:37:43 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -e
 set -x
-docker run --platform=linux/amd64 -v $(pwd)/patches:/patches -v $(pwd)/script:/script -v /result:/result -it build_unity_webrtc_plugin_android ./script/runner.sh all
+docker run --platform=linux/amd64 -v $(pwd)/script:/script -v /result:/result -it build_unity_webrtc_plugin_android ./script/runner.sh all
