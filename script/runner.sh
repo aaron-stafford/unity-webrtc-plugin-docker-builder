354b7da9b1fbcbcb939f16320118d3441655f5d8
DrStaf <dark-apple@nacious.net>
2023-06-09 21:42:11 -0400

    Update runner.sh

--- script/runner.sh
+++ script/runner.sh
@@ -5,6 +5,6 @@ echo Cloning repo: $WEBRTC_GIT_REPO_BRANCH
 echo Cloning fork: $WEBRTC_GIT_REPO
 git clone --branch $WEBRTC_GIT_REPO_BRANCH $WEBRTC_GIT_REPO --depth=1
 cd com.unity.webrtc
-cp result/webrtc-android.zip webrtc.zip
+cp ../../result/webrtc-android.zip webrtc.zip
 bash -e BuildScripts~/build_plugin_android.sh
 cp Runtime/Plugins/Android/libwebrtc.aar /result/.
